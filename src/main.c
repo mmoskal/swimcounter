17e3c0706d0e51002c6605fde04eee904fc8f6e6
Michał Moskal <michal.moskal@microsoft.com>
2016-02-14 00:25:47 -0800

    Works + icons

--- src/main.c
+++ src/main.c
@@ -2,30 +2,27 @@
 
 static Window *window;
 static TextLayer *text_layer;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Select");
-}
+static TextLayer *number_layer;
+static TextLayer *time_layer;
+static ActionBarLayer *action_bar;
 
 static int aboveZeroStart = -1, phaseMax, phaseMaxAt, tryPhaseMaxAt, lastCount, lastHigh;
-static int count, sampleNo;
+static int count, sampleNo, lastTime;
 
 static void showCount() {
-  // Long lived buffer
-  static char s_buffer[20];
-
-  // Compose string of all data for 3 samples
-  snprintf(s_buffer, sizeof(s_buffer), 
-    "%d", count
-  );
-    
-    text_layer_set_text(text_layer, s_buffer);
+    static char s_buffer[10];
+    static char s_time[10];
+    snprintf(s_buffer, sizeof(s_buffer), "%d", count);
+    text_layer_set_text(number_layer, s_buffer);
+    snprintf(s_time, sizeof(s_time), "%02d:%02d", lastTime/60, lastTime % 60);
+    text_layer_set_text(time_layer, s_time);
 }
 
 static uint64_t startTime;
 static int ind = 0;
 
-
+//#define DBG(args...) APP_LOG(APP_LOG_LEVEL_DEBUG, args)
+#define DBG(args...)
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
     // we need a peek 150ms wide and at least 1300 high
@@ -57,7 +54,7 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
                 ind = 1000;
             }
             if (tryPhaseMaxAt && now - aboveZeroStart > 150) {
-                APP_LOG(APP_LOG_LEVEL_DEBUG, "Real phase max, %d", (int)(now - aboveZeroStart));
+                DBG("Real phase max, %d", (int)(now - aboveZeroStart));
                 phaseMaxAt = tryPhaseMaxAt;
                 tryPhaseMaxAt = 0;
                 ind = 1500;
@@ -67,7 +64,7 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
             aboveZeroStart = -1;
             ind = 0;
             if (x < -1600) {
-                APP_LOG(APP_LOG_LEVEL_DEBUG, "d0=%d d1=%d", now - lastHigh, now - phaseMaxAt);
+                DBG("d0=%d d1=%d", now - lastHigh, now - phaseMaxAt);
                 if (now - phaseMaxAt < 1500) {
                     // do not count too often
                     if (now - lastCount > 1500) {
@@ -76,27 +73,34 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
                         count = count + 1;
                         phaseMax = 0;
                         vibes_short_pulse();
+                        lastTime = (now - 10000) / 1000;
                         showCount();
                     }
                 }
             }
         }
         
-        APP_LOG(APP_LOG_LEVEL_DEBUG, "ACCEL: %d,%d,%d,%d", ++sampleNo, now, x, ind);
+        sampleNo++;
+        DBG("ACCEL: %d,%d,%d,%d", sampleNo, now, x, ind);
     }
 }
 
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+    count = 0;
+    showCount();
+}
+
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Counting");
+    text_layer_set_text(text_layer, "Go ahead!");
     
     uint32_t num_samples = 25;
     accel_data_service_subscribe(num_samples, data_handler);
     accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(text_layer, "Down");
-    count = 0;
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {    
+    text_layer_set_text(text_layer, "Cheating...");
+    count++;
     showCount();
 }
 
@@ -106,35 +110,70 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
-static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+static GBitmap *icon0, *icon1, *icon2;
 
-  text_layer = text_layer_create(GRect(5, 0, bounds.size.w - 10, bounds.size.h));
-  text_layer_set_text(text_layer, "Press UP");
-  text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+static void window_load(Window *window) {
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    text_layer = text_layer_create(GRect(5, 0, bounds.size.w - 10, 60));
+    text_layer_set_text(text_layer, "UP to start");
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer_set_overflow_mode(text_layer, GTextOverflowModeWordWrap);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+
+    number_layer = text_layer_create(GRect(5, 60, bounds.size.w - 10, bounds.size.h));
+    text_layer_set_text(number_layer, "");
+    text_layer_set_text_alignment(number_layer, GTextAlignmentCenter);
+    text_layer_set_font(number_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+
+    time_layer = text_layer_create(GRect(5, bounds.size.h - 35, bounds.size.w - 10, 35));
+    text_layer_set_text(time_layer, "00:00");
+    text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+    text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    layer_add_child(window_layer, text_layer_get_layer(number_layer));
+    layer_add_child(window_layer, text_layer_get_layer(time_layer));
     
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    
+    action_bar = action_bar_layer_create();
+    action_bar_layer_add_to_window(action_bar, window);
+    action_bar_layer_set_click_config_provider(action_bar, click_config_provider);
+
+    // Set the icons:
+    // The loading of the icons is omitted for brevity... See gbitmap_create_with_resource()
+    icon0 = gbitmap_create_with_resource(RESOURCE_ID_IMG_TRASH);
+    icon1 = gbitmap_create_with_resource(RESOURCE_ID_IMG_SETTINGS);
+    icon2 = gbitmap_create_with_resource(RESOURCE_ID_IMG_PLAY);
+    
+    action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_UP, icon2, true);
+    action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_SELECT, icon0, true);
+    action_bar_layer_set_icon_animated(action_bar, BUTTON_ID_DOWN, icon1, true);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    text_layer_destroy(text_layer);
+    gbitmap_destroy(icon0);
+    gbitmap_destroy(icon1);
+    gbitmap_destroy(icon2);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-	.load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+    window = window_create();
+    window_set_click_config_provider(window, click_config_provider);
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    
+    const bool animated = true;
+    window_stack_push(window, animated);
 }
 
 static void deinit(void) {
     window_destroy(window);
+    accel_data_service_unsubscribe();
 }
 
 int main(void) {
