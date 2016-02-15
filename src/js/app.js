d622dbaf3c47011ca5302a350f1f8ae6bb3be881
Michał Moskal <michal.moskal@microsoft.com>
2016-02-15 15:21:06 -0800

    cloud-saving

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+function postCsv(buf, cb) {
+    var client = new XMLHttpRequest();
+    client.onreadystatechange = function () {
+        if (client.readyState == 4) {
+            console.log("Server response", client.status, client.responseText);
+            cb(client.status);
+        }
+    };
+    client.open("POST", "https://nct.azurewebsites.net/data");
+    client.setRequestHeader("content-type", "text/csv; charset=utf-8");
+    client.send(buf);
+}
+
+function decode(arr) {
+  var out = "";
+  var ind = 0;
+  function g(off,k) {
+    var v = (arr[off+1] << 8) | (arr[off]);
+    ind += ((v >> 13) & 7) << k;
+    return (v << 19) >> 19;
+  }
+  for(var i = 0; i < arr.length; i += 6) {
+    ind = 0;
+    var x = g(i+0, 0);
+    var y = g(i+2, 3);
+    var z = g(i+4, 6);
+    out += x + "," + y + "," + z + "," + ind + "\n";
+  }
+  return out;
+}
+
+function sendMsg(dict) {
+    var name = Object.keys(dict).join(",");
+    Pebble.sendAppMessage(dict, function(e) {
+        console.log('Sent message, ' + name);
+    }, function(e) {
+        console.log('Send failed, message, ' + name);
+    });
+}
+
+Pebble.addEventListener('ready', function(e) {
+    console.log('ProCounter JS ready');
+    sendMsg({ 'KEY_APP_READY':1 });
+});
+
+var csv = "";
+
+Pebble.addEventListener('appmessage', function(e) {
+    var data = e.payload.KEY_ACC_DATA || [];
+    csv += decode(data);
+    
+    if (e.payload.KEY_POST_DATA && csv) {
+        var tosend = "X,Y,Z,I\n" + csv;
+        csv = "";
+        postCsv(tosend, function(status) {
+            sendMsg({ KEY_DATA_POSTED: status });
+        });
+    }
+});
--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <pebble.h>
 
+
+static int *nullPtr;
+static int assert_handler(const char *cond, const char *fn, int line) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Assertion failed: %s:%d: %s is false!", fn, line, cond);
+    return 1;
+}
+
+#define assert(x) ((void)(!(x) && assert_handler(#x, __FILE__, __LINE__) && (*nullPtr = 42, 1)))
+
 static Window *window;
 static TextLayer *text_layer;
 static TextLayer *number_layer;
@@ -8,6 +17,88 @@ static ActionBarLayer *action_bar;
 
 static int phaseStop, phaseLen, aboveZeroStart = -1, phaseMax, phaseMaxAt, tryPhaseMaxAt, lastCount;
 static int count, sampleNo, lastTime;
+static int recordingStatus;
+
+#define PKTSIZE 14
+
+typedef struct DataPacket {
+    struct DataPacket *next;
+    int byteSize;
+    int16_t payload[PKTSIZE*3];
+} *Pkt;
+
+static int numRetries, sendState;
+static Pkt queue;
+
+#define KEY_ACC_DATA 1
+#define KEY_APP_READY 2
+#define KEY_POST_DATA 3
+#define KEY_DATA_POSTED 4
+
+static void sendFirst() {
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "sending first, rs=%d", recordingStatus);
+    
+    sendState = 1;
+    DictionaryIterator *iterator;
+    app_message_outbox_begin(&iterator);   
+    if (recordingStatus == 2) {
+        dict_write_int8(iterator, KEY_POST_DATA, 1);
+        recordingStatus = 3;
+    }
+    dict_write_data(iterator, KEY_ACC_DATA, (const uint8_t*)&queue->payload, queue->byteSize);
+    app_message_outbox_send();
+}
+
+static void pokeSend() {
+    if (sendState != 2) return;
+    if (recordingStatus == 0 || recordingStatus == 3) return;
+    if (queue)
+        sendFirst();
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Message received!");
+    Tuple *data = dict_find(iterator, KEY_APP_READY);
+    if (data && sendState == 0) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "Got ready msg fom phone; will stream data now");
+        sendState = 2;
+        pokeSend();
+    }
+    
+    data = dict_find(iterator, KEY_DATA_POSTED);
+    if (data) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Data posted: %d", (int)data->value->int32);
+        if (data->value->int32 == 200)
+            text_layer_set_text(text_layer, "Saved!");
+        else
+            text_layer_set_text(text_layer, "Error saving.");
+        recordingStatus = 0;
+    }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+    if (numRetries++ < 10) {
+        sendFirst();
+    }
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    //APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+    numRetries = 0;
+    Pkt p = queue;
+    queue = p->next;
+    free(p);
+    if (queue) {
+        sendFirst();
+    } else {
+        sendState = 2;
+    }
+}
 
 static void showCount() {
     static char s_buffer[10];
@@ -25,8 +116,10 @@ static int ind = 0;
 #define DBG(args...)
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
-    char hexbufA[num_samples * 4 + 25];
-    char hexbufB[num_samples * 4 + 25];
+    assert(num_samples <= PKTSIZE);
+    
+    Pkt pkt = (Pkt)malloc(sizeof(struct DataPacket));
+    int ptr = 0;
     
     // we need a peek 150ms wide and at least 1300 high
     // followed by a value under -1600 less than 1500ms after the peak
@@ -88,16 +181,38 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
         
         sampleNo++;
         
-        unsigned int mid = num_samples / 2;
-        char *ptr =  i >= mid ? &hexbufB[(i - mid) * 8] : &hexbufA[i * 8];
-        #define R(v,s) (((v >> 4) & 0x3ff) << s)
-        snprintf(ptr, 9, "%08x", R(data[i].x, 0) |  R(data[i].y, 10) | R(data[i].z, 20) | (ind << 30));
+        #define R(f,k) pkt->payload[ptr++] = ((data[i].f) & 0x1fff) | (((ind >> k) & 0x7) << 13)
+        
+        R(x,0);
+        R(y,3);
+        R(z,6);
         
         DBG("ACCEL: %d,%d,%d,%d", sampleNo, now, x, ind);
     }
     
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "HXTC: %s", hexbufA);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "HXTC: %s", hexbufB);
+    pkt->next = NULL;
+    pkt->byteSize = ptr * 2;
+    
+    if (queue == NULL) {
+        queue = pkt;
+        pokeSend();
+    } else {
+        Pkt last = queue;
+        int depth = 0;
+        while (last->next) {
+            last = last->next;
+            depth++;
+        }        
+        last->next = pkt;
+        if (depth > 200) {
+            pkt = queue;
+            queue = queue->next;
+            free(pkt);
+        }
+    }
+    
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "HXTC: %s", hexbufA);
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "HXTC: %s", hexbufB);
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -108,15 +223,28 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     text_layer_set_text(text_layer, "Go ahead!");
     
-    uint32_t num_samples = 20;
+    
+    APP_LOG(APP_LOG_LEVEL_INFO, "Mem free: %d", (int)heap_bytes_free());
+    
+    
+    uint32_t num_samples = PKTSIZE;
     accel_data_service_subscribe(num_samples, data_handler);
     accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {    
-    text_layer_set_text(text_layer, "Cheating...");
-    count++;
-    showCount();
+    if (sendState == 0) return;
+    
+    if (recordingStatus == 0) {
+        text_layer_set_text(text_layer, "Recording...");    
+        recordingStatus = 1;
+        pokeSend();
+    } else if (recordingStatus == 1) {
+        text_layer_set_text(text_layer, "Saving...");    
+        recordingStatus = 2;
+        pokeSend();
+    } else {
+    }
 }
 
 static void click_config_provider(void *context) {
@@ -178,6 +306,15 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
+    // Register callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+
+    // Open AppMessage with sensible buffer sizes
+    app_message_open(126, 126);
+
     window = window_create();
     window_set_click_config_provider(window, click_config_provider);
     window_set_window_handlers(window, (WindowHandlers) {
@@ -192,6 +329,12 @@ static void init(void) {
 static void deinit(void) {
     window_destroy(window);
     accel_data_service_unsubscribe();
+    app_message_deregister_callbacks();
+    while (queue) {
+        Pkt tmp = queue;
+        queue = queue->next;
+        free(tmp);
+    }
 }
 
 int main(void) {
